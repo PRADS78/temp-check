8ca990c76864da3ec222ba146488a9514f587485
Pradeep G <pradeep.g@disprz.com>
2025-06-29 14:59:07 +0530

    sdsd

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "*.js": [
-    "eslint --cache --fix --cache-location=./node_modules/.cache/eslint/.eslintcache",
-    "jest --coverage=false --bail --findRelatedTests",
-  ],
-  "*.{scss,css}":
-    "stylelint --cache --fix --cache-location=./node_modules/.cache/stylelint/.stylelintcache",
-};
